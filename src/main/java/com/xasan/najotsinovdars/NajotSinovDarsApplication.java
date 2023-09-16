09a42d777e6a2716d7902b320b8250951ec289dc
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2023-09-16 20:58:04 +0500

    Initial commit

---
+++ src/main/java/com/xasan/najotsinovdars/NajotSinovDarsApplication.java
@@ -0,0 +1,13 @@
+package com.xasan.najotsinovdars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NajotSinovDarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NajotSinovDarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xasan/najotsinovdars/NajotSinovDarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xasan.najotsinovdars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NajotSinovDarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
